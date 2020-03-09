3454a7a1a4c6f7792ebcbb84db097708e2895e2e
zeefola <zeefola@gmail.com>
2020-03-09 16:09:21 +0100

    Worked on footer router link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,7 +8,7 @@
                             <div class="cta_wrapper">
                                 <div class="ttm-quicklink-box">
                                    <div class="ttm-leftlink-box">
-                                       <h5 class="custom-heading"><a href="#"> <span class="ti ti-arrow-circle-left ttm-arrow-box"></span> Find Our Nearest Office</a></h5>
+                                       <h5 class="custom-heading"><a href="#"> <span class="ti ti-arrow-circle-left ttm-arrow-box"></span> Find Our Nearest Shop</a></h5>
                                        <i class="flaticon flaticon-house ttm-textcolor-skincolor"></i>
                                   </div>    
                                 </div>
@@ -18,15 +18,15 @@
                             <div class="ttm-custom-ctabox">
                                 <div class="cta-bg-icon"><span class="flaticon-support"></span></div>
                                 <h3 class="ttm-cta-heading ttm-textcolor-skincolor">Call Us On</h3>
-                                <h5 class="ttm-cta-content">+123 456 789 0012</h5>
+                                <h5 class="ttm-cta-content">+234 812 262 7008</h5>
                             </div>
                         </div>
                         <div class="col-lg-4">
                             <div class="cta_wrapper">
                                 <div class="ttm-quicklink-box">
                                    <div class="ttm-rightlink-box">
-                                        <i class="flaticon flaticon-clock ttm-textcolor-skincolor"></i>
-                                        <h5 class="custom-heading"><a href="#"> Emergency Restorations <span class="ti ti-arrow-circle-right ttm-arrow-box"></span></a></h5>
+                                        <i class="flaticon flaticon-support ttm-textcolor-skincolor"></i>
+                                        <h5 class="custom-heading">Help Center <span class="ti ti-arrow-circle-right ttm-arrow-box"></span></a></h5>
                                   </div>    
                                 </div>
                             </div>
@@ -41,10 +41,9 @@
                             <div class="widget widget_text  clearfix">
                                 <h3 class="widget-title"> About Nardus</h3>
                                 <div class="">
-                                    <p>Nardus is bundled Html Template for who is passionate about farming and agriculture, gardening services related websites.</p>
-                                    <p>For everyone who is passionate about agro services, farm nurseries, farming & gardening tools suppliers and blogs.</p>
+                                    <p>Nardus is the next big name in the essential oils industry. We blaze the trail as the leading local supplier of essential oils in Nigeria, poised to meet the dynamic demands of the global market.</p>
                                     <hr>
-                                    <a class="ttm-btn ttm-btn-size-sm ttm-btn-color-skincolor btn-inline ttm-icon-btn-right" href="#">Find Us On Map  <i class="ti ti-arrow-circle-right"></i></a>
+                                    <!-- <a class="ttm-btn ttm-btn-size-sm ttm-btn-color-skincolor btn-inline ttm-icon-btn-right" href="#">Find Us On Map  <i class="ti ti-arrow-circle-right"></i></a> -->
                                 </div>
                             </div>
                         </div>
@@ -52,12 +51,10 @@
                             <div class="widget link-widget clearfix">
                                <h3 class="widget-title">Useful Links</h3>
                                 <ul id="menu-footer-services">
-                                    <li><a href="#">About Us</a></li>
-                                    <li><a href="#">Our Services</a></li>
-                                    <li><a href="#">Nomadic Herding</a></li>
-                                    <li><a href="#">Veterinary Services</a></li>
-                                    <li><a href="#">Commercial Plant</a></li>
-                                    <li><a href="#">Farm Labor Management</a></li>
+                                    <router-link tag="li" to="/about"><a> About </a></router-link>
+                                    <router-link to="/faq" tag="li"> <a>  Faq </a> </router-link>
+                                    <router-link to="/shop" tag="li"> <a> Shop </a> </router-link>
+                                    <router-link to="/contact" tag="li"><a> Contact Us </a></router-link>
                                 </ul>
                             </div>
                         </div>
@@ -87,16 +84,15 @@
                             <div class="widget contact-widget">
                                 <h3 class="widget-title">Get in touch</h3>      
                                 <ul class="contact-widget-wrapper">
-                                    <li><i class="fa fa-map-marker"></i>4789 Melmorn Street,Zakila Ton <br> Mashintron Town</li>
-                                    <li><i class="fa fa-phone"></i>(+01) 123 456 7890 </li>
-                                    <li><i class="fa fa-envelope-o"></i><a href="mailto:info@example.com" target="_blank">info@example.com</a></li>
+                                    <li><i class="fa fa-map-marker"></i>18 Udi street, Wema Terrace <br> Osborne Foreshore, Ikoyi</li>
+                                    <li><i class="fa fa-phone"></i> (234) 906-282-2260 </li>
+                                    <li><i class="fa fa-envelope-o"></i><a href="mailto:info@nardus.ng" target="_blank">info@nardus.ng</a></li>
                                 </ul>
                                 <div class="social-icons circle social-hover">
                                     <ul class="list-inline">
                                         <li class="social-facebook"><a class="tooltip-top" target="_blank" href="#" data-tooltip="Facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                                        <li class="social-twitter"><a class="tooltip-top" target="_blank" href="#" data-tooltip="Twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                                        <li class="social-flickr"><a class=" tooltip-top" target="_blank" href="#" data-tooltip="flickr"><i class="fa fa-flickr" aria-hidden="true"></i></a></li>
-                                        <li class="social-linkedin"><a class=" tooltip-top" target="_blank" href="" data-tooltip="LinkedIn"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                                        <li class="social-twitter"><a class="tooltip-top" target="_blank" href="https://twitter.com/nardusltd" data-tooltip="Twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                                        <li class="social-instagram"><a class=" tooltip-top" target="_blank" href="https://www.instagram.com/nardusltd/" data-tooltip="instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
                                     </ul>
                                 </div>
                             </div>
