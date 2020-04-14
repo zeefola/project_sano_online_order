888ef3b889d456291a90b3021126a4eedb8d231f
Martins Abiodun <martinsabiodun94@gmail.com>
2020-04-14 09:46:21 +0100

    added cart.js module

---
+++ src/store/cart.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+
+
+namespaced: true
+const state = {
+    count: 100,
+}
+const getters = {   }
+const mutations = { 
+    // incremenent(state){
+    //     state.count++;
+    // }
+}
+const actions = {
+
+
+
+
+
+}
+
+export default {
+    state,
+    getters,
+    mutations,
+    actions,
+}
+
+
+
+
+
+
